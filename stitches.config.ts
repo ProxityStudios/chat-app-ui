e832da13a40a1353d46615fce74543806aa85b4f
CrawlTheDev <hamzaadamis@icloud.com>
2022-08-26 22:05:46 -0400

    feat: implement Inter font

--- stitches.config.ts
+++ stitches.config.ts
@@ -67,7 +67,7 @@ export const globalStyles = globalCss({
   "html, body": {
     colorScheme: "dark",
     fontFamily:
-      "-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif",
+      "Inter, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif",
   },
   "html, body, #__next": {
     background: "$background",
